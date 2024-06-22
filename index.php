575f246bf5e2f84b318b4809f3c3990e615446dd
Aogiri Izumi <aogiriizumi@gmail.com>
2024-06-22 17:34:18 +0700

    Add files via upload
    https://akaridesu.000webhostapp.com/

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cek Khodam</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Cek Khodam</h2>
+        <form action="khodam.php" method="post">
+            <label for="nama">Nama Pengguna:</label><br>
+            <input type="text" id="nama" name="nama"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+</html>
